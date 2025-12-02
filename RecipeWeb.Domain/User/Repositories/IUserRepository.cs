37748333fca1512b532c2cc39453e4b6de84a067
Daniil <rychenkov16@yandex.ru>
2025-12-02 21:36:37 +0300

    create user entity and interface repository

---
+++ RecipeWeb.Domain/User/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace RecipeWeb.Domain.User.Repositories;
+
+public interface IUserRepository
+{
+    Task<User> GetByIdAsync(int id);
+    Task<User> GetByFirstNameAsync(string firstname);
+    Task<User> GetByLoginAsync(string login);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(User user);
+    Task<bool> ExistsByLoginAsync(string login);
+    Task<bool> ExistsByFirstNameAsync(string firstname);
+    Task<IEnumerable<User>> GetAllAsync();
+}   
--- RecipeWeb.Domain/User/User.cs
+++ RecipeWeb.Domain/User/User.cs
@@ -2,5 +2,44 @@
 
 public class User
 {
+    public Guid Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string Login { get; private set; }
+    public string Password { get; private set; }
+    public string Description { get; private set; }
+    
+    private User(){}
+
+    public static User Create(string firstName, string login, string password, string description)
+    {
+        return new User
+        {
+            Id = Guid.NewGuid(),
+            FirstName = firstName,
+            Login = login,
+            Password = password,
+            Description = description
+        };
+    }
+
+    public void UpdateFirstName(string newFirstname)
+    {
+        FirstName = newFirstname;
+    }
+
+    public void UpdateLogin(string newLogin)
+    {
+        Login = newLogin;
+    }
+
+    public void UpdatePasswordHash(string newPassword)
+    {
+        Password = newPassword;
+    }
+    
+    public void UpdateDescription(string newDescription)
+    {
+        Description = newDescription;
+    }
     
 }
