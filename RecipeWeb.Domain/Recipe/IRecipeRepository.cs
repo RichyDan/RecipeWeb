86d5d1e0fcc0dd3c90a2013cea880be852acb558
ryche <rychenkov16@yandex.ru>
2026-02-16 23:16:22 +0300

    add recipe

---
+++ RecipeWeb.Domain/Recipe/IRecipeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeWeb.Domain.Recipe;
+
+public interface IRecipeRepository
+{
+    Task<User.User> GetByIdAsync(int id);
+    Task<User.User> FindByFirstNameAsync(string firstname);
+    Task<User.User> FindByLoginAsync(string login);
+    Task AddAsync(User.User user);
+    Task UpdateAsync(User.User user);
+    Task DeleteAsync(User.User user);
+}   
--- RecipeWeb.Domain/Recipe/Recipe.cs
+++ RecipeWeb.Domain/Recipe/Recipe.cs
@@ -1,6 +1,41 @@
-﻿namespace RecipeWeb.Domain.Recipe;
+﻿using RecipeWeb.Domain.Common;
 
-public class Recipe
+namespace RecipeWeb.Domain.Recipe;
+
+public class Recipe : Entity
 {
+    public Recipe (
+        string name, 
+        string description,
+        string timeToCook, 
+        string countPersons,
+        string imageUrl)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        TimeToCook = timeToCook;
+        CountPersons = countPersons;
+        ImageUrl = imageUrl;
+    }
     
+    public string Name { get; private set; }
+    public string TimeToCook { get; private set; }
+    public string CountPersons { get; private set; }
+    public string Description { get; private set; }
+    public string ImageUrl { get; private set; }
+    
+    public void Update(
+        string name, 
+        string description, 
+        string timeToCook, 
+        string countPersons,
+        string imageUrl)
+    {
+        Name = name;
+        Description = description;
+        TimeToCook = timeToCook;
+        CountPersons = countPersons;
+        ImageUrl = imageUrl;
+    }
 }
