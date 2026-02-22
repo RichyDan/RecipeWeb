19dd465e52cbd449a06c7850bbabad7605f608e7
ryche <rychenkov16@yandex.ru>
2026-02-22 23:57:14 +0300

    newCommit

--- RecipeWeb.Domain/Recipe/IRecipeRepository.cs
+++ RecipeWeb.Domain/Recipe/IRecipeRepository.cs
@@ -2,5 +2,9 @@
 
 public interface IRecipeRepository
 {
-    
+    Task<Recipe> GetByIdAsync(int id);
+    Task<IEnumerable<Recipe>> GetAllAsync();
+    Task AddAsync(Recipe recipe);
+    Task UpdateAsync(Recipe recipe);
+    Task DeleteAsync(int id);
 }
--- RecipeWeb.Domain/Recipe/Recipe.cs
+++ RecipeWeb.Domain/Recipe/Recipe.cs
@@ -1,6 +1,69 @@
-﻿namespace RecipeWeb.Domain.Recipe;
+﻿using RecipeWeb.Domain.Common;
 
-public class Recipe
+namespace RecipeWeb.Domain.Recipe;
+
+public class Recipe : Entity
 {
+    public Recipe (
+        string name, 
+        string description,
+        string timeToCook, 
+        string countPersons,
+        string imageUrl)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        TimeToCook = timeToCook;
+        CountPersons = countPersons;
+        ImageUrl = imageUrl;
+        
+        Ingredients = new List<Ingredient>(); // Инициализация пустых списков
+        Tags = new List<Tag>();
+        Steps = new List<Step>();
+    }
     
+    public string Name { get; private set; }
+    public string TimeToCook { get; private set; }
+    public string CountPersons { get; private set; }
+    public string Description { get; private set; }
+    public string ImageUrl { get; private set; }
+    public List<Ingredient> Ingredients { get; private set; }
+    public List<Step> Steps { get; private set; }
+    public List<Tag> Tags { get; private set; }
+    
+    public void Update(
+        string name, 
+        string description, 
+        string timeToCook, 
+        string countPersons,
+        string imageUrl,
+        List<Ingredient> newIngredients = null,
+        List<Tag> newTags = null,
+        List<Step> newSteps = null)
+    {
+        Name = name;
+        Description = description;
+        TimeToCook = timeToCook;
+        CountPersons = countPersons;
+        ImageUrl = imageUrl;
+        
+        if (newIngredients != null)
+        {
+            Ingredients.Clear();
+            Ingredients.AddRange(newIngredients);
+        }
+
+        if (newTags != null)
+        {
+            Tags.Clear();
+            Tags.AddRange(newTags);
+        }
+
+        if (newSteps != null)
+        {
+            Steps.Clear();
+            Steps.AddRange(newSteps);
+        }
+    }
 }
