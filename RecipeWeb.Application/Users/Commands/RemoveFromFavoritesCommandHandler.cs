8684292351cddd8f0ef6b96f361e35b828026a24
ryche <rychenkov16@yandex.ru>
2026-06-23 21:50:26 +0300

    правки

--- RecipeWeb.Application/Users/Commands/RemoveFromFavoritesCommandHandler.cs
+++ RecipeWeb.Application/Users/Commands/RemoveFromFavoritesCommandHandler.cs
@@ -1,14 +1,18 @@
 using RecipeWeb.Application.Common.Interfaces;
+using RecipeWeb.Domain.RecipeAggregate;
 using RecipeWeb.Domain.UserAggregate;
 
 namespace RecipeWeb.Application.Users.Commands;
 
-public class RemoveFromFavoritesCommandHandler(IUserRepository userRepository) : ICommandHandler<RemoveFromFavoritesCommand>
+public class RemoveFromFavoritesCommandHandler(IUserRepository userRepository, IRecipeRepository recipeRepository) : ICommandHandler<RemoveFromFavoritesCommand>
 {
     public async Task Handle(RemoveFromFavoritesCommand command, CancellationToken cancellationToken)
     {
         User user = await userRepository.GetByIdAsync(command.UserId)
-            ?? throw new KeyNotFoundException($"Пользователь с Id {command.UserId} не найден");
+            ?? throw new InvalidOperationException($"Пользователь с Id {command.UserId} не найден");
+
+        if (await recipeRepository.GetByIdAsync(command.RecipeId) is null)
+            throw new InvalidOperationException($"Рецепт с Id {command.RecipeId} не найден");
 
         user.RemoveFromFavorites(command.RecipeId);
     }
